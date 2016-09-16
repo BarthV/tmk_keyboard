abd841c5865d8dcf49cf5d879b404575905e03c1
Barthelemy Vessemont <b.vessemont@criteo.com>
2016-09-17 01:32:44 +0200

    nnkb 2x2 works :)

---
+++ keyboard/nnkb_2x2/keymap_nnkb2x2.c
@@ -0,0 +1,10 @@
+#include "keymap_common.h"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* 0: A B C Maj */
+    KEYMAP(Q,  B, \
+           LSFT, C),
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+};
