b519a585cc1f76b0d3fa0ca5564ada8907296467
Barthelemy Vessemont <b.vessemont@criteo.com>
2016-09-18 00:34:08 +0200

    backlight still broken

--- keyboard/nnkb_2x2/mcuconf.h
+++ keyboard/nnkb_2x2/mcuconf.h
@@ -41,13 +41,7 @@
 /*
  * PWM driver system settings.
  */
-#define KINETIS_PWM_USE_ADVANCED              FALSE
-#define KINETIS_PWM_USE_TIM1                  TRUE
-#define KINETIS_PWM_USE_TIM2                  FALSE
-#define KINETIS_PWM_USE_TIM3                  FALSE
-#define KINETIS_PWM_TIM1_IRQ_PRIORITY         3
-#define KINETIS_PWM_TIM2_IRQ_PRIORITY         3
-#define KINETIS_PWM_TIM3_IRQ_PRIORITY         3
+#define KINETIS_PWM_USE_FTM0 TRUE
 
 /*
  * SERIAL driver system settings.
