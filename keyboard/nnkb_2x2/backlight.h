6e95fb6c00ba792ca148c3b0593944e7487e4b21
Barthelemy Vessemont <b.vessemont@criteo.com>
2016-09-17 17:55:24 +0200

    backlight broken + dos2unix

---
+++ keyboard/nnkb_2x2/backlight.h
@@ -0,0 +1,10 @@
+#ifndef BACKLIGHT_H
+#define BACKLIGHT_H
+
+void backlight_init_ports(void);
+
+void backlight_switch_invert(void);
+void backlight_switch_enable(void);
+void backlight_switch_disable(void);
+
+#endif // BACKLIGHT_H
