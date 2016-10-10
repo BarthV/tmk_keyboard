4bdeb2821869d5db45cb2368146b9a1c9718150a
Barthelemy Vessemont <b.vessemont@criteo.com>
2016-10-10 10:08:24 +0200

    nnkb2x2 OK

--- keyboard/nnkb_2x2/keymap_nnkb2x2.c
+++ keyboard/nnkb_2x2/keymap_nnkb2x2.c
@@ -1,10 +1,29 @@
 #include "keymap_common.h"
+#include "backlight.h"
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* 0: A B C Maj */
-    KEYMAP(Q,  B, \
-           LSFT, C),
+    /* 0:
+     * .-----.-----.
+     * |  Q  |  B  |
+     * :-----+-----:
+     * | Maj | FN0 |
+     * '-----'-----'
+     */
+    KEYMAP(   Q, B, \
+           LSFT, FN0),
+    /* 1 (Fn0):
+     * .-----.-----.
+     * | Bst | No  |
+     * :-----+-----:
+     * | BTo | FN0 |
+     * '-----'-----'
+     */
+    KEYMAP(FN2, NO, \
+           FN1, FN0)
 };
 
-const uint16_t PROGMEM fn_actions[] = {
+const action_t PROGMEM fn_actions[] = {
+  [0] = ACTION_LAYER_MOMENTARY(1),
+  [1] = ACTION_BACKLIGHT_TOGGLE(),
+  [2] = ACTION_BACKLIGHT_STEP()
 };
