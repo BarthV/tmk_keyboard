630cdc0394477b0d7ff9d5de1f5bb9b457441b9e
Barthelemy Vessemont <b.vessemont@criteo.com>
2016-11-07 23:09:52 +0100

    work in progress

--- keyboard/nnkb_numpad/config.h
+++ keyboard/nnkb_numpad/config.h
@@ -52,7 +52,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 )
 
 /* number of backlight levels */
-#define BACKLIGHT_LEVELS 11
+#define BACKLIGHT_LEVELS 10
 
 /*
  * Feature disable options
